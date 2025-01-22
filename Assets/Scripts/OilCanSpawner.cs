2451da2c9824fa5bc48565dd383a45e846e7a7aa
hojoomoni <drsisilyt@gmail.com>
2025-01-22 15:06:07 +0900

    기름통 스포너 구현

---
+++ Assets/Scripts/OilCanSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class OilCanSpawner : MonoBehaviour
+{
+    public GameObject oilCanPrefab;
+    public Transform[] spawnPoints;
+    public float spawnInterval = 5f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnOilCanRepeatedly());
+    }
+
+    IEnumerator SpawnOilCanRepeatedly()
+    {
+        while (true)
+        {
+            SpawnOilCan();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void SpawnOilCan()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        Transform selectedSpawnPoint = spawnPoints[randomIndex];
+
+        if (selectedSpawnPoint != null)
+        {
+            Instantiate(oilCanPrefab, selectedSpawnPoint.position, selectedSpawnPoint.rotation);
+        }
+    }
+}
